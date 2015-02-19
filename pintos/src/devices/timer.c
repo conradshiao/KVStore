fc0d9ff317ba7612aca7ddaf1e3167e60cdf107e
Conrad Shiao <conrad.shiao@berkeley.edu>
2015-02-18 22:46:40 -0800

    timer

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -92,8 +92,11 @@ timer_sleep (int64_t ticks)
   int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+  thread_block();
+  while (timer_elapsed (start) < ticks) {
+    continue;
+  }
+  thread_unblock();
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
