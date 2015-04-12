d8d20d947a82760239e8fc2f9d8188bd59087efa
Victor Yuan <yuanshizhan2011@gmail.com>
2015-04-12 19:15:29 +0000

    testing

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -395,7 +395,7 @@ load (const char *cmdline, void (**eip) (void), void **esp)
 
 done:
   /* We arrive here whether the load is successful or not. */
-  //file_close (file);
+  file_close (file);
   // OUR CODE HERE
   palloc_free_page(cmdline_copy);
   return success;
